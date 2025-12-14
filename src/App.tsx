c3190f5684e1ddf24d5e7b7726f1abfe8421bc05
Krishna kumar <krishsrkv@gmail.com>
2025-12-14 13:52:23 +0400

    Build the Navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <Grid
       templateAreas={{
-        base: `"nav nav"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"nav" "main"`, // mobile screen
+        lg: `"nav nav" "aside main"`, // large screen
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+      gap={4}
     >
       <GridItem area="nav" bg="red.300">
-        Nav
+        <Navbar />
       </GridItem>
+
       <Show above="lg">
         <GridItem area="aside" bg="yellow.300">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="blue.300">
+
+      <GridItem area="main" bg="blue.300" height="100vh">
         Main
       </GridItem>
     </Grid>
