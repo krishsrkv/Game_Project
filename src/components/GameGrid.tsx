4d047c7b7a3de764b49b4cb915b55b2e166d0dd7
Krishna kumar <krishsrkv@gmail.com>
2025-12-14 20:51:45 +0400

    Create Custom HooksFor Fetching Game

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,7 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => {
-        console.log("API Response:", res.data); // 👈 Add this for debugging
-        setGames(res.data.results || []); // 👈 Fallback prevents undefined
-      })
-      .catch((err) => setError(err.message));
-  }, []);
-
-  if (error) return <p>{error}</p>;
-
+  const { games, error } = useGames();
   return (
     <ul>
       {games.map((game) => (
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+
+    const controller = new AbortController();
+    apiClient
+      .get<FetchGamesResponse>("/games", {signal : controller.signal})
+      .then((res) => {
+        console.log("API Response:", res.data); // 👈 Add this for debugging
+        setGames(res.data.results || []); // 👈 Fallback prevents undefined
+      })
+      .catch((err) => setError(err.message));
+      return() => controller.abort();
+  }, []);
+
+return {games,error};
+
+
+
+}
+
+export default useGames
